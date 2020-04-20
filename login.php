48b91519925bcb7dfc23d08ccd467e904a154d69
hot17ch <hot17ch@gmail.com>
2020-04-20 19:07:02 +0900

    게시판 로그인 수정

--- login.php
+++ login.php
@@ -1,25 +1,24 @@
 <!DOCTYPE html>
 <html lang="ko" dir="ltr">
 <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="layout.css">
-        <title>게시판</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="layout.css">
+  <title>게시판</title>
 </head>
 <body>
-        <div align='center'>
-        <span>로그인</span>
-
-        <form method='get' action='login_action.php'>
-                <p>ID: <input name="id" type="text"></p>
-                <p>PW: <input name="pw" type="password"></p>
-                <input type="submit" value="로그인">
-        </form>
-        <br />
-        <button id="join" onclick="location.href='./join.php'">회원가입</button>
-
-        </div>
-
-
+  <div class="login">
+    <span>로그인</span>
+    <form method='get' action='login_action.php' autocomplete='off'>
+      <ul class="clearfix">
+        <li class="floatLeft">ID</li>
+        <li class="floatLeft"><input name="id" type="text"></li>
+        <li class="floatLeft">PW</li>
+        <li class="floatLeft"><input name="pw" type="password"></li>
+      </ul>
+      <input type="submit" value="로그인">
+    </form>
+    <br />
+    <button id="join" onclick="location.href='./join.php'">회원가입</button>
+  </div>
 </body>
-
 </html>
